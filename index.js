e7f765818eb490b359f708b643849c4ddb0059ba
humorous-storage-7521 <rmwerner@umich.edu>
2020-08-26 12:14:06 -0500

    make like button work

--- index.js
+++ index.js
@@ -6,25 +6,45 @@ const playerContainer = document.querySelector(".player-container")
 function renderPlayer(player) {
   // create an element for the outer div
   const playerDiv = document.createElement("div")
-
+  
   // set attributes on the outer div
   playerDiv.className = "player"
   playerDiv.dataset.number = player.number
-
+  
   // use innerHTML to create any child elements of the div
   playerDiv.innerHTML = `
-    <h3>${player.name} (<em>${player.nickname}</em>)</h3>
-    <img src="${player.photo}" alt="${player.name}">
-    <p class="likes">${player.likes} likes</p>
-    <button class="like-button">❤️</button>
+  <h3>${player.name} (<em>${player.nickname}</em>)</h3>
+  <img src="${player.photo}" alt="${player.name}">
+  <p class="likes">${player.likes} likes</p>
+  <button id="${player.number}" class="like-button">❤️</button>
   `
+//   let button = document.querySelector(`div[data-number="${player.number}"] button`)
+//   console.log(button)
+//   button.addEventListener('click', function() {
+//     player.likes += 1
+// })
 
-  // append the element to the container
-  playerContainer.append(playerDiv)
+playerContainer.append(playerDiv)
 }
+
 
 // for each player in the array, render to the DOM
 PLAYERS.forEach(renderPlayer)
+
+
+function makeLikeButtonWork(player) {
+  let button = document.getElementById(`${player.number}`)
+  button.addEventListener('click', function() {
+    player.likes += 1
+    let showLikes = document.querySelector(`div[data-number="${player.number}"] p`)
+    showLikes.innerHTML = player.likes + " likes"
+  })
+}
+
+
+PLAYERS.forEach(makeLikeButtonWork)
+
+
 
 /***** End of Starter Code ****/
 
@@ -39,8 +59,25 @@ function toggleColor(element) {
     element.style.color = "red"
   }
 }
+let header = document.querySelector("h1#header")
+header.addEventListener("click", function(e) {
+  toggleColor(header)
+})
+
 
 
 /***** Deliverable 2 *****/
-
+let form = document.getElementById('new-player-form')
+form.addEventListener('submit', function() {
+  event.preventDefault()
+  let player =  {
+    number: form.number.valueAsNumber,
+    name: form.name.value,
+    nickname: form.nickname.value,
+    photo: form.photo.value,
+    likes: 0
+  }
+  renderPlayer(player)
+  makeLikeButtonWork(player)
+})
 /***** Deliverable 3 *****/
